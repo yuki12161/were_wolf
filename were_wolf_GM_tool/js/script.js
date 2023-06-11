50803a8a27277d3e8d43522b89a01e19c235dfa7
Yuki Sato <yuki.sato12161216@gmail.com>
2023-06-11 17:26:24 +0900

    汎用的に改修

--- were_wolf_GM_tool/js/script.js
+++ were_wolf_GM_tool/js/script.js
@@ -1,60 +1,43 @@
 function check_winner() {
-  let village_cnt = 10;
-  let wolf_cnt = 3;
-  let fox_cnt = 1;
+  let cnt_v = 0;
+  let cnt_w = 0;
+  let cnt_f = 0;
 
-  // 村陣営
-  for (let i = 0; i < document.village.c1.length; i++) {
-    if (document.village.c1[i].checked || document.village.c2[i].checked) {
-      village_cnt--;
+  let table = document.getElementById('tbl');
+
+  for (let row of table.rows) {
+    c1 = row.cells[3].children.c1.checked;
+    c2 = row.cells[4].children.c2.checked;
+
+    // 生存者をカウント
+    if (!c1 && !c2) {
+      if (row.classList.contains("village") == true) {
+        cnt_v++;
+      } else if(row.classList.contains("wolf") == true) {
+        cnt_w++;
+      } else if(row.classList.contains("fox") == true) {
+        cnt_f++;
+      }
     }
   }
 
-  // 狂信者
-  const v1_1 = document.getElementById('v1_1');
-  const v1_2 = document.getElementById('v1_2');
-  if (v1_1.checked || v1_2.checked) {
-    village_cnt--;
-  }
+  console.log('村カウント：' + cnt_v);
+  console.log('狼カウント：' + cnt_w);
+  console.log('狐カウント：' + cnt_f);
 
-  // 背徳者
-  const v2_1 = document.getElementById('v2_1');
-  const v2_2 = document.getElementById('v2_2');
-  if (v2_1.checked || v2_2.checked) {
-    village_cnt--;
-  }
-
-  // 狼
-  for (let i = 0; i < document.wolf.c1.length; i++) {
-    if (document.wolf.c1[i].checked || document.wolf.c2[i].checked) {
-      wolf_cnt--;
-    }
-  }
-
-  // 狐
-  const f1 = document.getElementById('f1');
-  const f2 = document.getElementById('f2');
-  if (f1.checked || f2.checked) {
-    fox_cnt--;
-  }
-
-  console.log('村カウント：' + village_cnt);
-  console.log('狼カウント：' + wolf_cnt);
-  console.log('狐カウント：' + fox_cnt);
-
-  if (wolf_cnt == 0) {
-    if (fox_cnt == 0) {
+  if (cnt_w == 0) {
+    if (cnt_f == 0) {
       alert('村人陣営の勝利です')
     } else {
       alert('妖狐陣営の勝利です（村勝利条件成立）')
     }
-  } else if (village_cnt <= wolf_cnt) {
-    if (fox_cnt == 0) {
+  } else if (cnt_v <= cnt_w) {
+    if (cnt_f == 0) {
       alert('人狼陣営の勝利です')
     } else {
       alert('妖狐陣営の勝利です（狼勝利条件成立）')
     }
   } else {
-    alert('ゲーム続行');
+    alert('ゲーム続行\n\n■生存者\n・村：' + cnt_v + '\n・狼：' + cnt_w + '\n・狐：' + cnt_f);
   }
 }
