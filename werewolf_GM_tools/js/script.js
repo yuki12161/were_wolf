64aaecbf6d1962fe9468c9d76b529520ded50b83
Yuki Sato <yuki.sato12161216@gmail.com>
2023-06-21 17:43:18 +0900

    リファクタ

--- werewolf_GM_tools/js/script.js
+++ werewolf_GM_tools/js/script.js
@@ -1,4 +1,29 @@
-function check_winner() {
+function judge_winner() {
+
+  counter = count_player()
+  cnt_v = counter[0]
+  cnt_w = counter[1]
+  cnt_f = counter[2]
+
+  if (cnt_w == 0) {
+    if (cnt_f == 0) {
+      alert('村人陣営の勝利です')
+    } else {
+      alert('妖狐陣営の勝利です（村勝利条件成立）')
+    }
+  } else if (cnt_v <= cnt_w) {
+    if (cnt_f == 0) {
+      alert('人狼陣営の勝利です')
+    } else {
+      alert('妖狐陣営の勝利です（狼勝利条件成立）')
+    }
+  } else {
+    alert('ゲーム続行\n\n■生存者\n・村：' + cnt_v + '\n・狼：' + cnt_w + '\n・狐：' + cnt_f);
+  }
+}
+
+function count_player() {
+
   let cnt_v = 0;
   let cnt_w = 0;
   let cnt_f = 0;
@@ -23,39 +48,5 @@ function check_winner() {
       row.classList.add("row_gray")
     }
   }
-
-  console.log('村カウント：' + cnt_v);
-  console.log('狼カウント：' + cnt_w);
-  console.log('狐カウント：' + cnt_f);
-
-  if (cnt_w == 0) {
-    if (cnt_f == 0) {
-      alert('村人陣営の勝利です')
-    } else {
-      alert('妖狐陣営の勝利です（村勝利条件成立）')
-    }
-  } else if (cnt_v <= cnt_w) {
-    if (cnt_f == 0) {
-      alert('人狼陣営の勝利です')
-    } else {
-      alert('妖狐陣営の勝利です（狼勝利条件成立）')
-    }
-  } else {
-    alert('ゲーム続行\n\n■生存者\n・村：' + cnt_v + '\n・狼：' + cnt_w + '\n・狐：' + cnt_f);
-  }
-}
-
-function grayout() {
-   let table = document.getElementById('tbl');
-
-  for (let row of table.rows) {
-    c1 = row.cells[3].children.c1.checked;
-    c2 = row.cells[4].children.c2.checked;
-
-    if (!c1 && !c2) {
-      row.classList.remove("row_gray")
-    } else {
-      row.classList.add("row_gray")
-    }
-  }
+  return [cnt_v, cnt_w, cnt_f]
 }
